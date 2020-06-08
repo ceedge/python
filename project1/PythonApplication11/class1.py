926c32c2772ce817738e55d4806f403250df32c5
ceedge85 <ceedge85@gmail.com>
2020-06-08 01:27:31 -0400

    doing more with less

---
+++ project1/PythonApplication11/class1.py
@@ -0,0 +1,27 @@
+class class1(object):
+    """description of class""" 
+     
+
+## menuoption= int(input("1 - add  2 - subtract 3 -  muitplation 4 - divude:  "  ))  
+
+  #  a= int(input("first number:" ))
+ #   b= int(input("Second number:"))
+
+  #  if(menuoption == 1):
+      #  print( a + b )
+ #   elif(menuoption == 2):
+       # print(a-b)
+  #  elif(menuoption == 3):
+      #  print(a * b)
+  #  elif(menuoption == 4):
+      #  print(a / b)
+  #  else:
+     #   print("you typed something unexpected")
+
+"""
+x= input(" please type number:")
+print (int(x))
+print( "absulte value:" , abs(int(x)))
+""" 
+
+print("hello")
